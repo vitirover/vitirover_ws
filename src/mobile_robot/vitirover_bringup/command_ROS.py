172ca96b7a4ed265a403b32eeb046502902913a8
mathieugalle <mathieu.galle@ensc.fr>
2024-05-15 15:01:53 +0200

    added a basic command law that works to go left and right by blocking / decelerating the back wheel

--- src/mobile_robot/vitirover_bringup/command_ROS.py
+++ src/mobile_robot/vitirover_bringup/command_ROS.py
@@ -31,7 +31,7 @@ def cmd_vel_callback(data):
 
 # Socket setup
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-sock.bind(("YOUR.IP.HERE", 5005))
+sock.bind(("192.168.2.106", 5005))
 sock.setblocking(0)
 
 # Initialize ROS
@@ -45,10 +45,10 @@ rate = rospy.Rate(10)
 running = True
 count = 0
 count1 = 0
+
+angle_value = 0
+
 while running and not rospy.is_shutdown():
-    data, _ = sock.recvfrom(20000)
-    telemetry_data = telemetry_pb2.VitiroverTelemetry()
-    telemetry_data.ParseFromString(data)
     # Robot motion simulation logic
     
     R = x / (math.tan(omega * dt) + 0.00001)
@@ -57,31 +57,65 @@ while running and not rospy.is_shutdown():
     wH = (v/r) - omega * e/(2*r)
     wI = (v/r) * (1 - e/(2*R))
     wJ = (v/r) * (1 + e/(2*R))
-
-    angle_value = telemetry_data.back_axle_angle
-    #print("angle value: ", angle_value)
     
     if v == 0:
         wG = wH = wI = wJ = 0
 
-    if v < 0:
-        wG = -wG 
-        wH = -wH
+    # if v < 0:
+    #     wG = -wG
+    #     wH = -wH
+    #     wI = -wI
+    #     wJ = -wJ
 
-    #print('wG: ', wG)
-    #print('wH: ', wH)
-    #print('wI: ', wI)
-    #print('wJ: ', wJ)
+    print("omega : " + str(omega))
+    print('wG: ', wG)
+    print('wH: ', wH)
+    print('wI: ', wI)
+    print('wJ: ', wJ)
 
-    # Send to your robot over socket
+    # Send to your robot over socket    # if v < 0:
+    #     wG = -wG
+    #     wH = -wH
+    #     wI = -wI
+    #     wJ = -wJ
+
+    # This is a really simple command law to take into account the back axle value
+    # Feel free to test other command laws
+
+    robot_wants_to_go_right = angle_value < -15
+    robot_wants_to_go_left = angle_value > 15
+    if abs(omega) < 0.0001:
+        pass
+    elif omega < 0:
+        print("going right")
+
+        if robot_wants_to_go_right:
+            wI =  wI / 2.0
+        else:
+            wI = 0
+    elif omega > 0:
+        print("going left")
+        if robot_wants_to_go_left:
+            wJ = wJ / 2.0
+        else:
+            wJ = 0
+
     order = telemetry_pb2.VitiroverOrder()
     order.low_level_order.front_left_speed = int(wG * 8)
     order.low_level_order.front_right_speed = int(wH * 8)
     order.low_level_order.back_left_speed = int(wI * 8)
     order.low_level_order.back_right_speed = int(wJ * 8)
 
+    print(order)
+
     data = order.SerializeToString()
     sock.sendto(data, ("192.168.2.42", 5005)) #Robot IP here
+    
+    # data, _ = sock.recvfrom(20000)
+    # telemetry_data = telemetry_pb2.VitiroverTelemetry()
+    # telemetry_data.ParseFromString(data)
+    # angle_value = telemetry_data.back_axle_angle
+    print("angle value: ", angle_value)
 
     telemetry_data = None
     while True:
@@ -93,6 +127,9 @@ while running and not rospy.is_shutdown():
             break
     if telemetry_data != None:
         telemetry_data.ParseFromString(data)
+        print(telemetry_data)
+        angle_value = telemetry_data.back_axle_angle
+
         # Afficher quelques valeurs
         #print("Motor Data:", telemetry_data.front_left_wheel.back_electromotive_force)
         #print("wG/wGBFM: ", wG/(telemetry_data.front_left_wheel.back_electromotive_force+0.0001))
